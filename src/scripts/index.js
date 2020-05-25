bb12665053537ca6a8034e721ab99e73ee187672
Constantine Zubarev <kv.zubarev@gmail.com>
2020-05-25 19:00:44 +0600

    add slider class & remove old variant

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,14 +2,22 @@ import slider from "./slider";
 import suggestionsGroup from "./suggestions-group";
 import modal from "./modal";
 import form from "./form";
+import SliderClass from "./slider-class";
 
 window.addEventListener("DOMContentLoaded", () => {
-  slider({
+  /*  slider({
     sliderContainer: "home-slider",
     slideItem: "home-slider__slide",
     dotsContainer: "home-slider__dots",
     dot: "home-slider__dot",
-  });
+  });*/
+
+  new SliderClass({
+    sliderContainer: "home-slider",
+    slideItem: "home-slider__slide",
+    dotsContainer: "home-slider__dots",
+    dot: "home-slider__dot",
+  }).init();
 
   suggestionsGroup();
 
