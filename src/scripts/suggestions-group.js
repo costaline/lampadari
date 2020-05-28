0a104d77d93412e70fada1cc3ef010c64ae41ef9
Constantine Zubarev <kv.zubarev@gmail.com>
2020-05-28 22:07:21 +0600

    fix response

--- src/scripts/suggestions-group.js
+++ src/scripts/suggestions-group.js
@@ -1,12 +1,20 @@
+import { debounce } from "@/scripts/utils";
+
 const suggestionsGroup = () => {
   const list = document.querySelector(".suggestions-group__list");
   const toggleButton = document.querySelector(
     ".suggestions-group__toggle-button"
   );
 
-  const listHeight = list.clientHeight;
-
   let isShowed = true;
+
+  let listHeight = list.clientHeight;
+
+  const debouncedCalcHeight = debounce(() => {
+    listHeight = list.clientHeight;
+
+    list.style.marginBottom = isShowed ? 0 : `${-listHeight}px`;
+  }, 10);
 
   const toggleBoxView = () => {
     isShowed = !isShowed;
@@ -19,6 +27,8 @@ const suggestionsGroup = () => {
   setTimeout(() => isShowed && toggleBoxView(), 3000);
 
   toggleButton.addEventListener("click", toggleBoxView);
+
+  window.addEventListener("resize", debouncedCalcHeight);
 };
 
 export default suggestionsGroup;
